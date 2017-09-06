a5a4e983209c40b9b564a63daa1ff50bc6158933
Kevin Zhang <kevin.zhang.13499@gmail.com>
2017-09-06 20:56:42 +0000

    separate into usable function

--- relay.py
+++ relay.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import time
-if __name__ = ('__main__'):
+
+def operate_relay(mode):
     CHANNEL_1=16
     CHANNEL_2=18
     CHANNEL_1_DEFAULT = GPIO.LOW
@@ -9,14 +10,20 @@ if __name__ = ('__main__'):
     GPIO.setwarnings(False)
     GPIO.setup(CHANNEL_1, GPIO.OUT)
     GPIO.setup(CHANNEL_2, GPIO.OUT)
-    mode = sys.argv[1]
     if(mode == "on"):
         GPIO.output(CHANNEL_1, GPIO.HIGH)
         GPIO.output(CHANNEL_2, GPIO.HIGH)
     elif(mode == "off"):
+        GPIO.output(CHANNEL_1, GPIO.LOW)
+        GPIO.output(CHANNEL_2, GPIO.LOW)
+    else:
         GPIO.output(CHANNEL_1, GPIO.HIGH)
         GPIO.output(CHANNEL_2, GPIO.HIGH)
         time.sleep(1)
         GPIO.output(CHANNEL_1, CHANNEL_1_DEFAULT)
         GPIO.output(CHANNEL_2, CHANNEL_2_DEFAULT)
+
+
+if __name__ = ('__main__'):
+    operate_relay(sys.argv[1])
         
